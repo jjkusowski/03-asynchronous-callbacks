bc0d7cd575112a42230c290eda6c14fb9e696d8c
jjkusowski <jjkusowski@yahoo.com>
2017-11-29 14:48:03 -0800

    test and functions both work for good data

--- lab-jeff/__test__/reader.test.js
+++ lab-jeff/__test__/reader.test.js
@@ -2,12 +2,14 @@
 
 const reader = require('../lib/reader');
 
+const paths = [`${__dirname}/../assets/huskies.txt`, `${__dirname}/../assets/lions.txt`, `${__dirname}/../assets/sounders.txt`];
+
 describe('reader.js', () => {
   describe('reader.readText', () => {
     test('function should return contents of three files in order they are listed in the array passed in if there are no errors', (done) => {
-      reader.readText((error, data) => {
+      reader.readText(paths, (error, data) => {
         expect(error).toBeNull();
-        expect(data).toBeEqual(['The Michigan Tech Huskies are my favorite college team.', 'The Detroit Lions are my favorite NFL team.', 'The Seattle Sounders are my favorite soccer team.']);
+        expect(data).toEqual([`The Michigan Tech Huskies are my favorite college team.\n`, `The Detroit Lions are my favorite NFL team.\n`, `The Seattle Sounders are my favorite soccer team.\n`]);
         done();
       });
     });
---
+++ lab-jeff/lib/reader.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const reader = module.exports = {};
+const fs = require('fs');
+
+const dataArray = [];
+
+reader.readText = (paths, callback) => {
+  fs.readFile(paths[0], (error, data) => {
+    if(error) callback(error);
+    dataArray.push(data.toString());
+    fs.readFile(paths[1], (error, data) => {
+      if(error) callback(error);
+      dataArray.push(data.toString());
+      fs.readFile(paths[2], (error, data) => {
+        if(error) callback(error);
+        dataArray.push(data.toString());
+        callback(null, dataArray);
+      });
+    });
+  });
+
+};
